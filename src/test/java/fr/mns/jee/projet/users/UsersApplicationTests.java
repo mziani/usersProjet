191b5d8b1beccd74ca3e35602fd513f5c1b600dd
mziani <mohammed.ziani.pro@gmail.com>
2022-04-13 23:39:53 +0200

    initialisation

---
+++ src/test/java/fr/mns/jee/projet/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mns.jee.projet.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
