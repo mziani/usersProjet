ad6e6c6dc575ae519608347f55279ac90d47195f
Tai-Mui <chenh.tm@gmail.com>
2022-05-17 13:30:57 +0200

    [user] CRUD

--- src/main/java/fr/mns/jee/projet/model/User.java
+++ src/main/java/fr/mns/jee/projet/model/User.java
@@ -45,7 +45,7 @@ public class User implements Serializable {
 	@Column(name = "username",unique = true)
 	private String username;
 
-	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	//@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@Column(name = "password")
 	private String password;
 
